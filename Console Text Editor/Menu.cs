ae208dff556e00b18ea3c9e60433cab8f227570a
nasibullo03 <nasibullo.khalilov@bk.ru>
2022-06-08 21:23:05 +0700

    Был создан класс Menu и добавлены некоторые функции для отображения пунктов меню. Извлек базовый класс TextEditor  из класса Program.

---
+++ Console Text Editor/Menu.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Text_Editor
+{
+    internal class Menu
+    {
+        public enum Operation { Open, Create, OpenAndEdit, FileIndexing, MainMenu, MenuOpenFile, MenuFileIndexing, Edit, Save, Back }
+        
+        public static Operation CurrentOperation { get; set; }
+
+        public class Show
+        {
+            public static void MainMenu()
+            {
+                PrintColorText("Вводите команду:\n", ConsoleColor.Green);
+                PrintColorText($"O - открыть файл\n"
+                    + $"С - создать файл\n"
+                    + $"E - открыть и редактировать файл\n"
+                    + $"I - выпольнить индексатсию файлов по ключевым словам\n"
+                    , ConsoleColor.Yellow, ConsoleColor.Red
+                    );
+                Clear();
+
+            }
+            static void Menu_OpenFile()
+            {
+                Clear();
+                PrintColorText("Введите путь к файлу.\n" +
+                    "Например: D:/FileName.FileFormat\n" +
+                    "Path: ",
+                    ConsoleColor.Green);
+                DefaultConsoleColor();
+
+
+
+            }
+            static void Menu_CreateFile()
+            {
+
+            }
+            static void Menu_OpenAndCreate()
+            {
+
+            }
+            static void Menu_FileIndexing()
+            {
+
+            }
+
+
+        }
+        
+        public static ConsoleKeyInfo GetKey()
+        {
+            return Console.ReadKey();
+        }
+
+
+        #region PerformOperation
+        public static void PerformKey(Operation operation, ConsoleKeyInfo keyInfo)
+        {
+            switch (operation)
+            {
+                case Operation.Open:
+                    break;
+                case Operation.Create:
+                    break;
+                case Operation.OpenAndEdit:
+                    break;
+                case Operation.FileIndexing:
+                    break;
+                case Operation.MainMenu:
+                    break;
+                case Operation.MenuOpenFile:
+                    break;
+                case Operation.MenuFileIndexing:
+                    break;
+                case Operation.Edit:
+                    break;
+                case Operation.Save:
+                    break;
+                case Operation.Back:
+                    break;
+                default:
+                    break;
+
+            }
+
+
+
+        }
+        private static void PerformOperation_Open(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_Create(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_MainMenu(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_OpenAndEdit(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_FileIndexing(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_Edit(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_Save(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        private static void PerformOperation_Back(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.I:
+                    break;
+            }
+        }
+        #endregion 
+
+        
+        #region Output Management
+        private static void PrintColorText(
+            string text,
+            ConsoleColor foregroundColor = ConsoleColor.Gray,
+            ConsoleColor backgroundColor = ConsoleColor.Black)
+        {
+            Console.ForegroundColor = foregroundColor;
+            Console.BackgroundColor = backgroundColor;
+            Console.Write(text);
+        }
+        private static void DefaultConsoleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        public static void Clear()
+        {
+            Console.Clear();
+            DefaultConsoleColor();
+        }
+        #endregion
+
+    }
+}
---
+++ Console Text Editor/TextEditor.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TextEditor
+{
+    /// <summary>
+    /// Изменить текст
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string Edit(string text)
+    {
+        return ReadLine(text);
+    }
+
+    private static void GoToDefautPosition(List<char> chars, int defPosX, int startPosition, int defPosY = 0)
+    {
+        if (defPosY == 0)
+        {
+            if (startPosition < 0) startPosition = 0;
+            if (startPosition < chars.Count)
+            {
+                for (int i = startPosition; i < chars.Count; ++i)
+                    Console.Write(chars[i]);
+            }
+
+        }
+        else
+        {
+            int textBuferSize = Console.BufferWidth * (defPosY + 1);
+            int lastColumnFilledSpace = (chars.Count + 7) % Console.BufferWidth;
+            int lastColumnEmptySpace = Console.BufferWidth - lastColumnFilledSpace;
+            int lastColumnEndTextPosiotion = textBuferSize - lastColumnEmptySpace;
+
+            startPosition = lastColumnEndTextPosiotion - (lastColumnFilledSpace - startPosition);
+            if (startPosition < chars.Count)
+            {
+                for (int i = startPosition; i < chars.Count; ++i)
+                    Console.Write(chars[i]);
+            }
+        }
+
+
+
+        Console.SetCursorPosition(++defPosX, defPosY);
+
+    }
+
+    private static string ReadLine(string Default)
+    {
+        int pos = Console.CursorLeft, defPos = Console.CursorLeft;
+        bool CursorLeftZero = false;
+        Console.Write(Default);
+        ConsoleKeyInfo info;
+        List<Char> chars = new List<char>();
+        if (string.IsNullOrEmpty(Default) == false)
+        {
+            chars.AddRange(Default.ToCharArray());
+        }
+        while (true)
+        {
+
+            info = Console.ReadKey();
+
+            if ((info.Key == ConsoleKey.Backspace) && (Console.CursorLeft >= 0) && chars.Count != 0 && ((Console.CursorTop == 0 && Console.CursorLeft > 6) || Console.CursorTop > 0))
+            {
+                if (Console.CursorLeft == 0 && !CursorLeftZero)
+                {
+                    chars.RemoveAt(chars.Count - 1);
+                    Console.Write(' ');
+                    Console.CursorLeft -= 1;
+                    CursorLeftZero = true;
+                }
+                else if (Console.CursorLeft == 0)
+                {
+                    chars.RemoveAt(chars.Count - 1);
+                    Console.CursorTop -= 1;
+                    Console.CursorLeft = Console.BufferWidth - 1;
+                    Console.Write(' ');
+                    Console.CursorLeft = Console.BufferWidth - 1;
+                    Console.CursorTop -= 1;
+                    CursorLeftZero = false;
+
+                }
+                else
+                {
+                    chars.RemoveAt(chars.Count - 1);
+                    Console.Write(' ');
+                    Console.CursorLeft -= 1;
+                }
+
+            }
+            else if ((info.Key == ConsoleKey.Backspace) && (Console.CursorTop >= 0 && Console.CursorLeft < 7))
+            {
+                Console.CursorLeft += 1;
+            }
+            else if (info.Key == ConsoleKey.Enter)
+            {
+                Console.SetCursorPosition(Console.BufferWidth - 1, (int)((chars.Count + 7) / Console.BufferWidth));
+                Console.Write(Environment.NewLine);
+                break;
+            }
+            ///------------RIGHT------------
+            else if (info.Key == ConsoleKey.RightArrow)
+            {
+
+                void GoDown()
+                {
+                    Console.CursorLeft = Console.BufferWidth - 3;
+                    defPos = Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: (Console.CursorTop == 0) ? 0 : --Console.CursorTop);
+                    Console.CursorTop++;
+                    Console.CursorLeft = 0;
+                }
+
+                int lastColumnFilledSpace = (chars.Count + 7) % Console.BufferWidth;
+
+                if ((Console.CursorTop == (int)((chars.Count + 7) / Console.BufferWidth) && Console.CursorLeft <= lastColumnFilledSpace + 1)
+                    || Console.CursorTop <= (int)((chars.Count + 7) / Console.BufferWidth))
+                {
+                    if (Console.CursorTop == (int)((chars.Count + 7) / Console.BufferWidth))
+                    {
+                        if (Console.CursorLeft >= lastColumnFilledSpace)
+                        {
+                            --Console.CursorLeft;
+                            continue;
+                        }
+                    }
+                    if (Console.CursorLeft == 0)
+                    {
+                        GoDown();
+                    }
+                    else
+                    {
+                        Console.CursorLeft -= (Console.CursorLeft <= 0) ? 0 : 1;
+                        defPos = Console.CursorLeft;
+                        GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: Console.CursorTop);
+                        Console.CursorLeft += 1;
+                    }
+                }
+                else if (Console.CursorTop <= (int)((chars.Count + 7) / Console.BufferWidth))
+                {
+                    defPos = Console.CursorLeft - 1;
+                    GoToDefautPosition(chars, defPos, defPos - 7, defPosY: Console.CursorTop);
+                    --Console.CursorLeft;
+                }
+                else if (Console.CursorTop == (int)((chars.Count + 7) / Console.BufferWidth + 1))
+                {
+                    --Console.CursorTop;
+                    Console.CursorLeft = Console.BufferWidth - 1;
+                }
+
+            }
+            ///------------LEFT------------
+            else if (info.Key == ConsoleKey.LeftArrow)
+            {
+                void GoToUP()
+                {
+                    Console.CursorLeft = Console.BufferWidth - 1;
+                    Console.CursorTop -= (Console.CursorTop > 0) ? 1 : 0;
+                    defPos = Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: Console.CursorTop);
+                    --Console.CursorLeft;
+                }
+                if (Console.CursorLeft > 8 && Console.CursorTop == 0)
+                {
+                    Console.CursorLeft -= (Console.CursorLeft - 2 <= 0) ? 0 : 2;
+                    defPos = Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: Console.CursorTop);
+
+                }
+                else if (Console.CursorTop > 0)
+                {
+                    if (Console.CursorLeft == 0)
+                    {
+                        GoToUP();
+                    }
+                    else
+                    {
+                        try
+                        {
+                            Console.CursorLeft -= 1;
+                        }
+                        catch
+                        {
+
+                            Console.CursorLeft = Console.BufferWidth - 1;
+                            Console.CursorTop -= (Console.CursorTop > 0) ? 1 : 0;
+                        }
+                        finally
+                        {
+                            defPos = Console.CursorLeft;
+                            GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: Console.CursorTop);
+
+                            if (Console.CursorLeft - 1 < 0) GoToUP();
+                            else --Console.CursorLeft;
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.CursorLeft -= 1;
+                    defPos = Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: Console.CursorTop);
+                }
+
+            }
+            ///------------UP------------
+            else if (info.Key == ConsoleKey.UpArrow)
+            {
+                int CursorTop = Console.CursorTop;
+                if (CursorTop == 0)
+                {
+                    defPos = --Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos - 1, defPos - 7, defPosY: Console.CursorTop);
+                }
+                else
+                {
+
+                    Console.CursorLeft = (Console.CursorTop - 1 == 0) ? (Console.CursorLeft < 8) ? 7 : Console.CursorLeft : Console.CursorLeft;
+                    --Console.CursorTop;
+                    defPos = Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos - 2, defPos - 7, defPosY: Console.CursorTop);
+                }
+                if (Console.CursorLeft < 7 && Console.CursorTop == 0)
+                {
+                    Console.CursorLeft = 7;
+                }
+
+            }
+            ///------------DOWN------------
+            else if (info.Key == ConsoleKey.DownArrow)
+            {
+                int CursorTop = Console.CursorTop;
+                int textBuferSize = Console.BufferWidth * (Console.CursorTop + 1);
+                int lastColumnFilledSpace = (chars.Count + 7) % Console.BufferWidth;
+
+                if (CursorTop == (int)((chars.Count + 7) / Console.BufferWidth - 1) && Console.CursorLeft >= lastColumnFilledSpace)
+                {
+                    Console.CursorLeft = lastColumnFilledSpace;
+                    GoToDefautPosition(chars, Console.CursorLeft - 1, Console.CursorLeft - 7, defPosY: Console.CursorTop);
+                    ++Console.CursorTop;
+                }
+                else if (CursorTop == (int)((chars.Count + 7) / Console.BufferWidth) && Console.CursorLeft >= lastColumnFilledSpace)
+                {
+                    Console.CursorLeft = lastColumnFilledSpace;
+                    GoToDefautPosition(chars, Console.CursorLeft - 1, Console.CursorLeft - 7, defPosY: Console.CursorTop);
+                }
+                else
+                {
+                    Console.SetCursorPosition((Console.CursorLeft == 0) ? 0 : Console.CursorLeft - 1, Console.CursorTop);
+                    defPos = Console.CursorLeft;
+                    GoToDefautPosition(chars, defPos, defPos - 7, defPosY: Console.CursorTop);
+                    defPos = Console.CursorLeft;
+                    Console.SetCursorPosition(Console.CursorLeft, ++Console.CursorTop);
+                    GoToDefautPosition(chars, defPos - 2, defPos - 7, defPosY: Console.CursorTop);
+
+                }
+            }
+            else if (char.IsLetterOrDigit(info.KeyChar) ||
+                char.IsWhiteSpace(info.KeyChar) ||
+                char.IsSeparator(info.KeyChar) ||
+                char.IsSymbol(info.KeyChar) ||
+                char.IsPunctuation(info.KeyChar) ||
+                char.IsSurrogate(info.KeyChar))
+            {
+                int letterPosition = Console.CursorLeft;
+                GoToDefautPosition(chars, letterPosition - 1, letterPosition - 8, defPosY: Console.CursorTop);
+
+                if (Console.CursorTop == 0) chars.Insert(letterPosition - 8, info.KeyChar);
+                else
+                {
+
+                    int textBuferSize = Console.BufferWidth * (Console.CursorTop + 1);
+                    int lastColumnFilledSpace = (chars.Count + 7) % Console.BufferWidth;
+                    int lastColumnEmptySpace = Console.BufferWidth - lastColumnFilledSpace;
+                    int lastColumnEndTextPosiotion = textBuferSize - lastColumnEmptySpace;
+
+                    letterPosition -= 8;
+                    letterPosition = lastColumnEndTextPosiotion - (lastColumnFilledSpace - letterPosition);
+                    chars.Insert(letterPosition, info.KeyChar);
+                }
+            }
+        }
+        return new string(chars.ToArray());
+    }
+}
